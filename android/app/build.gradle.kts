c786f3b206b6f351a487fa67a2383a586896a873
ismailkhan-hybrid-app-developer <ismail.khan@focusitservices.co.uk>
2026-03-10 15:19:48 +0500

    add app icons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 36
-        versionCode = flutter.versionCode
+        versionCode = 3
         versionName = flutter.versionName
     }
 
