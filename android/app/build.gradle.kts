bebdbf429074417d47d33cbcf3ecc5fe4adf80fe
ismailkhan-hybrid-app-developer <ismail.khan@focusitservices.co.uk>
2026-03-30 19:44:11 +0500

    build number upgrade to 4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = flutter.versionName
     }
 
